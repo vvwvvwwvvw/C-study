7277765b0368387d91567d86798d373bdd52e1c7
apc <apc@DESKTOP-6DPQC77>
2024-10-01 16:10:34 +0900

    초기 커밋.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+
+namespace ConsoleApp1
+{
+    public class MyList
+    {
+        private int[] data = { 1, 2, 3, 4, 5 };
+
+        public IEnumerator GetEnumerator()
+        {
+            int i = 0;
+            while (i < data.Length)
+            {
+
+                yield return data[i];
+                i++;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //(1) foreach 사용 하여 Iteration
+            var list = new MyList();
+
+            foreach (var item in list)
+            { 
+                Console.WriteLine(item);
+            }
+
+            // (2) 수동 Iteration
+            IEnumerator it = list.GetEnumerator();
+            it.MoveNext();
+            Console.WriteLine(it.Current);
+            it.MoveNext();
+            Console.WriteLine(it.Current);
+        }
+    }
+}
